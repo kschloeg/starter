d1c3d93eaf2ea3bd0590d574f18271b007797f21
Kirk Schloegel <kschloeg@gmail.com>
2024-12-24 09:18:03 -0600

    fix this

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -79,7 +79,7 @@ function App() {
       <table>
         <thead>
           <tr>
-            <th>RAMONA Name</th>
+            <th>First Name</th>
             <th>Last Name</th>
             <th>Email Address</th>
             <th>Phone</th>
