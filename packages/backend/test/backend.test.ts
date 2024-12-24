f8332b30b9c1ff1b2f17f202a37fbb380ec75825
Kirk Schloegel <kschloeg@gmail.com>
2024-12-24 09:36:58 -0600

    add frontend-deploy empty test

--- packages/backend/test/backend.test.ts
+++ packages/backend/test/backend.test.ts
@@ -1,18 +1,16 @@
-import * as cdk from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import * as Backend from "../lib/backend-stack";
-import { test } from "@jest/globals";
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Backend from '../lib/backend-stack';
+import { test } from '@jest/globals';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/backend-stack.ts
-test("SQS Queue Created", () => {
+test('SQS Queue Created', () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new Backend.BackendStack(app, "MyTestStack");
+  const stack = new Backend.BackendStack(app, 'MyTestStack');
   // THEN
   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties("AWS::SQS::Queue", {
+  template.hasResourceProperties('AWS::SQS::Queue', {
     VisibilityTimeout: 300,
   });
 });
